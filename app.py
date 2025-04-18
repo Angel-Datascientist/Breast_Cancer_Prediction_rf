78b4aab47a8bdeb278ba813b61ab415f9a489e59
Angeline Jacob <angelinejacob01@gmail.com>
2025-04-18 20:35:55 +0530

    deployment

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
-from prediction import prediction,feature_names
+from prediction import predict_result, feature_names
+
 
 st.title("Breast Cancer Prediction App (Random Forest)")
 st.write("Enter the tumor's measurements below:")
+
+
+
 
 input_data = []
 
@@ -13,7 +17,8 @@ with st.form("prediction_form"):
     submitted = st.form_submit_button("Predict")
 
 if submitted:
-    result = prediction(input_data)
+    result = predict_result(input_data)
     st.success(f"The model predicts: **{result}**")
+
 
 
--- prediction.py
+++ prediction.py
@@ -1,15 +1,6 @@
-import pandas as pd
+import numpy as np
 import pandas as pd
 from model import load_model
-
-input_data = (
-    17.99, 10.38, 122.8, 1001.0, 0.1184,
-    0.2776, 0.3001, 0.1471, 0.2419, 0.07871,
-    1.095, 0.9053, 8.589, 153.4, 0.006399,
-    0.04904, 0.05373, 0.01587, 0.03003, 0.006193,
-    25.38, 17.33, 184.6, 2019.0, 0.1622,
-    0.6656, 0.7119, 0.2654, 0.4601, 0.1189
-)
 
 feature_names = [ 
     'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean',
@@ -20,13 +11,9 @@ feature_names = [
     'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst', 'fractal_dimension_worst'
 ]
 
-input_df = pd.DataFrame([input_data], columns=feature_names)
+def predict_result(input_data):
+    model = load_model()
+    input_df = pd.DataFrame([input_data], columns=feature_names)
+    prediction = model.predict(input_df)
 
-
-prediction = load_model.predict(input_df)
-
-
-if prediction[0] == 0:
-    print("The person is not having cancer (Benign)")
-else:
-    print("The person is having cancer (Malignant)")
+    return "Malignant" if prediction[0] == 1 else "Benign"
